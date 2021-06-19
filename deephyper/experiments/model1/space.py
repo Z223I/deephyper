b0f78716404b2e15be75a12659a042afb089ba06
z223i <BruceWilsonUS@gmail.com>
2021-06-19 08:44:13 -0600

    Updated space.py.

--- deephyper/experiments/model1/space.py
+++ deephyper/experiments/model1/space.py
@@ -1,24 +1,15 @@
-from collections import OrderedDict
-from deephyper.benchmarks_hps.params import (activation, dropout, optimizer,
-                                         activation_flat_params,
-                                         optimizer_flat_params)
+"""Define exploration space."""
+#from collections import OrderedDict
+#from deephyper.benchmarks_hps.params import (activation, dropout, optimizer)
 from deephyper.search.models.base import param, step, Space
 
-epochs = param.discrete("epochs", 5, 500, step.ARITHMETIC, 1),
+epochs = param.discrete("epochs", 5, 100, step.ARITHMETIC, 1),
 batch_size = param.discrete("batch_size", 8, 1024, step.GEOMETRIC, 2),
 
 space = Space([
-    activation,
+    #activation,
     batch_size,
-    dropout,
+    #dropout,
     epochs,
-    optimizer
-])
-
-space_flat = Space([
-    *activation_flat_params,
-    batch_size,
-    dropout,
-    epochs,
-    *optimizer_flat_params
+    #optimizer
 ])
