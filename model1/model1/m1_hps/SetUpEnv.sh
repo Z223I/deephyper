b518e7bd8ab41a7ba646625185371f42c50c4ee3
z223i <BruceWilsonUS@gmail.com>
2021-07-05 13:32:22 -0600

    touch

--- model1/model1/m1_hps/SetUpEnv.sh
+++ model1/model1/m1_hps/SetUpEnv.sh
@@ -7,3 +7,7 @@ module load conda/deephyper/0.2.5
 
 # Activate conda env
 conda activate
+
+# This will allow you to run DeepHyper from anywhere.
+PATH_TO_ENV=/lus/theta-fs0/projects/datascience/wilsonb/theta/deephyper
+export PYTHONPATH=$PATH_TO_ENV:$PYTHONPATH
