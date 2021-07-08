f2a4e8d8072069aa820cd9c1c2e0c8326669bce3
z223i <BruceWilsonUS@gmail.com>
2021-07-08 13:52:27 -0600

    Updated config per DH run.

--- model1/model1/m1_hps/model_run_pytorch.py
+++ model1/model1/m1_hps/model_run_pytorch.py
@@ -480,17 +480,38 @@ def run(config):
 
 
 if __name__ == '__main__':
+    """
+activation                                           relu
+batch_size                                             32
+dropout1                                             0.05
+dropout2                                             0.05
+dropout3                                              0.1
+dropout4                                             0.05
+embed_hidden_size                                      21
+epochs                                                 20
+loss                                  binary_crossentropy
+omp_num_threads                                        64
+optimizer                                            Adam
+patience                                               12
+proportion                                            0.9
+units                                                  10
+id                   435e78da-e014-11eb-b3dc-e77839edcbe4
+objective                                        0.902046
+elapsed_sec                                       1250.48
+Name: 132, dtype: object
+    """
+    
     config = {
         'units': 10,
         'activation': 'relu',  # can be gelu
         'optimizer':  'AdamW',  # can be AdamW but it has to be installed.
         'loss':       'binary_crossentropy',
         'batch_size': 32,
-        'epochs':     15,
-        'dropout1':   0.20,
-        'dropout2':   0.10,
-        'dropout3':   0.05,
-        'dropout4':   0.00,
+        'epochs':     20,
+        'dropout1':   0.05,
+        'dropout2':   0.05,
+        'dropout3':   0.10,
+        'dropout4':   0.05,
         'patience':   12,           # Not currently used.
         'embed_hidden_size': 21,    # May not get used.
         'proportion': .90,          # A value between [0., 1.] indicating how to split data between
