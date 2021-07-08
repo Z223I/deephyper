0df9d9415b9e35d7f3e67d17cf9750423104a741
z223i <BruceWilsonUS@gmail.com>
2021-07-08 11:20:58 -0600

    ...

--- model1/model1/m1_hps/model_run_pytorch.py
+++ model1/model1/m1_hps/model_run_pytorch.py
@@ -360,7 +360,7 @@ def train(  args,
 
             predictedCpu = predictedCpu.cpu()
 
-            areEqual = np.equal(predictedCpu, Y_train)
+            areEqual = np.equal(predictedCpu.detach().numpy(), Y_train)
 
             # Get count of True elements in a numpy array
             acc = np.count_nonzero( areEqual ) / len( areEqual )
