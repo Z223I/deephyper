a8e57ea5d55b2432fda5d7d9ced9531bf26c536a
z223i <BruceWilsonUS@gmail.com>
2021-07-16 17:56:01 -0600

    Check-point.

---
+++ just_model.py
@@ -0,0 +1,109 @@
+
+## forward(x)
+## input_0 = x
+
+self.layer_norm = nn.LayerNorm(input_shape).to(device, dtype=dtype)
+##input_0 = self.layer_norm(input_0)
+
+#if batchSamples >= 16:
+in_features = numInputs
+out_features = 80
+self.dense = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+##dense = self.dense(input_0)
+self.dropout_1 = nn.Dropout2d(dropout1).to(device, dtype=dtype)
+##dense = self.dropout_1(dense)
+
+self.activation = torch.nn.Sigmoid().to(device, dtype=dtype)
+##activation = self.activation(dense)
+
+
+#if batchSamples >= 12:
+in_features = out_features
+out_features = 80
+self.dense_1 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+input_0 = activation
+##dense_1 = self.dense_1(input_0)
+self.dropout_2 = nn.Dropout2d(dropout2).to(device, dtype=dtype)
+##dense_1 = self.dropout_2(dense_1)
+
+
+# Add
+out_features = 1
+self.add = torch.nn.Sum().to(device, dtype=dtype)
+##add = self.add(activation, dense_1)
+
+out_features = 1
+self.activation_1 = torch.nn.ReLU().to(device, dtype=dtype)
+## Can use ReLU(inplace=False)
+##activation_1 = self.activation_1(add)
+
+#if batchSamples >= 10:
+in_features = out_features
+out_features = 96
+self.dense_2 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+##dense_2 = self.dense_2(activation_1)
+self.dropout_3 = nn.Dropout2d(dropout2).to(device, dtype=dtype)
+##dense_2 = self.dropout_3(dense_2)
+
+out_features = 1
+self.activation_2 = torch.nn.Tanh().to(device, dtype=dtype)
+##activation_2 = self.activation_2(dense_2)
+
+in_features = out_features
+out_features = 96
+self.dense_3 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+##dense_3 = self.dense_3(activation_1)
+self.dropout_4 = nn.Dropout2d(dropout3).to(device, dtype=dtype)
+##dense_3 = self.dropout_4(dense_3)
+
+out_features = 1
+self.add_1 = torch.nn.Sum().to(device, dtype=dtype)
+##add_1 = self.add_1(activation_2, dense_3)
+
+out_features = 1
+self.activation_3 = torch.nn.ReLU().to(device, dtype=dtype)
+## Can use ReLU(inplace=False)
+##activation_3 = self.activation_3(add_1)
+
+in_features = out_features
+out_features = 80
+self.dense_4 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+##dense_4 = self.dense_4(activation_3)
+self.dropout_5 = nn.Dropout2d(dropout4).to(device, dtype=dtype)
+##dense_4 = self.dropout_5(dense_4)
+
+out_features = 1
+self.activation_4 = torch.nn.Sigmoid().to(device, dtype=dtype)
+activation_4 = self.activation_4(dense_4)
+
+in_features = out_features
+out_features = 80
+self.dense_5 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+##dense_5 = self.dense_5(input_0)
+self.dropout_6 = nn.Dropout2d(dropout4).to(device, dtype=dtype)
+##dense_5 = self.dropout_6(dense_5)
+
+in_features = out_features
+out_features = 80
+self.dense_6 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+dense_6 = self.dense_6(activation_3)
+self.dropout_7 = nn.Dropout2d(dropout4).to(device, dtype=dtype)
+dense_6 = self.dropout_7(dense_6)
+
+out_features = 1
+self.add_2 = torch.nn.Sum().to(device, dtype=dtype)
+##add_2 = self.add_2(activation_4, dense_5, activation_1, dense_6)
+
+out_features = 1
+self.activation_5 = torch.nn.ReLU().to(device, dtype=dtype)
+### Can use ReLU(inplace=False)
+##activation_5 = self.activation_5(add_2)
+
+
+in_features = out_features
+out_features = 2 # This is 2 instead of 1 due to the PyTorch softmax.
+self.dense_7 = nn.Linear(in_features, out_features).to(device, dtype=dtype)
+dense_7 = self.dense_7(activation_5)
+
+#"input_layers": [["input_0", 0, 0]],
+#"output_layers": [["dense_7", 0, 0]]}
