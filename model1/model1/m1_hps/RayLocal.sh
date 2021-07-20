3b4c8b992896c25e2500f13c8138d84aead1a34c
z223i <BruceWilsonUS@gmail.com>
2021-07-20 09:25:57 -0600

    Checkpoint beffore major changes.

---
+++ model1/model1/m1_hps/RayLocal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+head_node=$HOSTNAME
+echo $HOSTNAME
+head_node_ip=$(dig $head_node a +short | awk 'FNR==2')
+echo ">$head_node_ip<"
+
+# if we detect a space character in the head node IP, we'll
+# convert it to an ipv4 address. This step is optional.
+if [[ "$head_node_ip" == *" "* ]]; then
+IFS=' ' read -ra ADDR <<<"$head_node_ip"
+if [[ ${#ADDR[0]} -gt 16 ]]; then
+head_node_ip=${ADDR[1]}
+else
+head_node_ip=${ADDR[0]}
+fi
+echo "IPV6 address detected. We split the IPV4 address as $head_node_ip"
+fi
+
+# Starting the Ray Head Node
+port=6379
+ip_head=$head_node_ip:$port
+export ip_head
+echo "IP Head: $ip_head"
+
+echo "Starting HEAD at $head_node"
+echo "ray start --head --node-ip-address=$head_node_ip --port=$port --block &"
+ray start --head --node-ip-address=$head_node_ip --port=$port --blockdeephyper nas random --evaluator ray --ray-address auto --problem deephyper.benchmark.nas.mnist1D.problem.Problem --max-evals 10 --num-cpus-per-task 6^ &
+
+# optional, though may be useful in certain versions of Ray < 1.0.
+sleep 10
---
+++ nas_problems/nas_problems/model1/README_DeepHyper_NAS_Running_Local.md
@@ -0,0 +1,6 @@
+# DeepHyper NAS Running Local
+
+
+```bash
+deephyper nas random --evaluator ray --ray-address auto --problem deephyper.benchmark.nas.mnist1D.problem.Problem --max-evals 10 --num-cpus-per-task 6
+```
