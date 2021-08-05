b2f991c26140be724bd7b13c9c55d6e1b502e53b
BruceRayWilson <BruceRayWilson@gmail.com>
2021-08-05 17:16:23 -0600

    touch

--- model1/model1/m1_hps/model_run_pytorch.py
+++ model1/model1/m1_hps/model_run_pytorch.py
@@ -211,10 +211,9 @@ class Model1(nn.Module):
         dense_6_t = torch.transpose(dense_5, 0, 1)
 
         print(f"activation_4.shape: {activation_4.shape}")
-        print(f"dense_5_t.shape: {dense_5_t.shape}")
-        print(f"activation_1.shape: {activation_1.shape}")
-        print(f"dense_6_t.shape: {dense_6_t.shape}")
-        add_2 = activation_4 + dense_5_t + activation_1 + dense_6_t
+        print(f"dense_5.shape: {dense_5.shape}")
+        print(f"dense_6.shape: {dense_6.shape}")
+        add_2 = activation_4 + dense_5 + dense_6
         # Can use ReLU(inplace=False)
         activation_5 = self.activation_5(add_2)
 
